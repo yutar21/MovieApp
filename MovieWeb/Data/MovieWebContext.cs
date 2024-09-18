64b60dfaa6c3b0d8444b1c9254962fc7a0aa1c51
Yutar <thanhduc111121@gmail.com>
2024-09-18 15:56:11 +0700

    firt commit

---
+++ MovieWeb/Data/MovieWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieWeb.Models;
+
+namespace MovieWeb.Data
+{
+    public class MovieWebContext : DbContext
+    {
+        public MovieWebContext (DbContextOptions<MovieWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieWeb.Models.Movie> Movie { get; set; } = default!;
+    }
+}
