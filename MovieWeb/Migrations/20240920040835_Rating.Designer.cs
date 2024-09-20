e0514a29eb44282aa8b2fd06a9c57d794054496d
Yutar <thanhduc111121@gmail.com>
2024-09-20 11:33:24 +0700

    fix controller

--- MovieWeb/Migrations/20240920040835_Rating.Designer.cs
+++ MovieWeb/Migrations/20240920040835_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieWeb.Data;
 
@@ -11,9 +12,11 @@ using MovieWeb.Data;
 namespace MovieWeb.Migrations
 {
     [DbContext(typeof(MovieWebContext))]
-    partial class MovieWebContextModelSnapshot : ModelSnapshot
+    [Migration("20240920040835_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MovieWeb/Program.cs
+++ MovieWeb/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MovieWeb.Data;
+using MovieWeb.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MovieWebContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MovieWebContext") ?? throw new InvalidOperationException("Connection string 'MovieWebContext' not found.")));
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<MovieWebContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
